7694f7d06b90f93a3796eea1a5fd65c22f1f6362
mrpaulwoods <mr.paul.woods@gmail.com>
2025-09-13 18:48:06 -0500

    feat: add initial project structure with proto definition and main class

---
+++ src/main/java/org/mrpaulwoods/Main.java
@@ -0,0 +1,12 @@
+package org.mrpaulwoods;
+
+import org.mrpaulwoods.models.PersonOuterClass;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+
+}
