9f87db64acc125c6019c87b2a5b052ca1cde6d30
mrpaulwoods <mr.paul.woods@gmail.com>
2025-09-14 10:22:43 -0500

    feat: add Lec04Composition to demonstrate usage of Address, Student, and School protobuf models

---
+++ src/main/java/org/mrpaulwoods/sec03/Lec04Composition.java
@@ -0,0 +1,34 @@
+package org.mrpaulwoods.sec03;
+
+import org.mrpaulwoods.models.sec03.Address;
+import org.mrpaulwoods.models.sec03.School;
+import org.mrpaulwoods.models.sec03.Student;
+import org.slf4j.Logger;
+
+public class Lec04Composition {
+    public static final Logger log = org.slf4j.LoggerFactory.getLogger(Lec04Composition.class);
+
+    public static void main(String[] args) {
+
+        var address = Address.newBuilder()
+                .setStreet("123 main st")
+                .setCity("Atlanta")
+                .setState("GA")
+                .build();
+
+        var student = Student.newBuilder()
+                .setName("sam")
+                .setAddress(address)
+                .build();
+
+        var school = School.newBuilder()
+                .setId(1)
+                .setName("high school")
+                .setAddress(address.toBuilder().setStreet("234 main st"))
+                .build();
+
+        log.info("student {}", student);
+        log.info("school {}", school);
+    }
+
+}
