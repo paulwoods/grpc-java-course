6361ec9d4c80ea648652238b1a2ffaf59912c7b6
mrpaulwoods <mr.paul.woods@gmail.com>
2025-09-14 21:02:26 -0500

    feat: add Lec02WellKnownTypes to demonstrate usage of well-known protobuf types including Int32Value and Timestamp

---
+++ src/main/java/org/mrpaulwoods/sec04/Lec02WellKnownTypes.java
@@ -0,0 +1,29 @@
+package org.mrpaulwoods.sec04;
+
+import com.google.protobuf.Int32Value;
+import com.google.protobuf.Timestamp;
+import org.mrpaulwoods.models.sec04.Sample;
+import org.slf4j.Logger;
+
+import java.time.Instant;
+
+public class Lec02WellKnownTypes {
+
+    public static final Logger log = org.slf4j.LoggerFactory.getLogger(Lec02WellKnownTypes.class);
+
+    public static void main(String[] args) {
+
+        var sample = Sample.newBuilder()
+                .setAge(Int32Value.of(12))
+                .setLoginTime(Timestamp.newBuilder()
+                        .setSeconds(Instant.now().getEpochSecond())
+                        .build());
+
+        log.info("sample: {}", sample);
+        log.info("has age: {}", sample.hasAge());
+        log.info("has loginTime: {}", sample.hasLoginTime());
+
+        log.info("time: {}", Instant.ofEpochSecond(sample.getLoginTime().getSeconds()));
+
+    }
+}
