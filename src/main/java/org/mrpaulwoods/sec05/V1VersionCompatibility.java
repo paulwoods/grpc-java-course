e9ff5926a9990d1940a640406cd97b284c66a65b
mrpaulwoods <mr.paul.woods@gmail.com>
2025-09-14 21:19:29 -0500

    feat: update Person model to make age field optional - java class will have "hasAge" method

---
+++ src/main/java/org/mrpaulwoods/sec05/V1VersionCompatibility.java
@@ -0,0 +1,13 @@
+package org.mrpaulwoods.sec05;
+
+import org.slf4j.Logger;
+
+public class V1VersionCompatibility {
+
+    public static final Logger log = org.slf4j.LoggerFactory.getLogger(V1VersionCompatibility.class);
+
+    public static void main(String[] args) {
+
+    }
+
+}
