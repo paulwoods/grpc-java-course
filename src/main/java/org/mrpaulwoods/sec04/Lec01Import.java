d9c66889bd438503b28084f1709418bd98d56e71
mrpaulwoods <mr.paul.woods@gmail.com>
2025-09-14 20:49:38 -0500

    feat: add Lec01Import to demonstrate protobuf imports and composition with Person, Address, and Car models

---
+++ src/main/java/org/mrpaulwoods/sec04/Lec01Import.java
@@ -0,0 +1,30 @@
+package org.mrpaulwoods.sec04;
+
+import org.mrpaulwoods.models.common.Address;
+import org.mrpaulwoods.models.common.BodyStyle;
+import org.mrpaulwoods.models.common.Car;
+import org.mrpaulwoods.models.sec04.Person;
+import org.slf4j.Logger;
+
+public class Lec01Import {
+
+    public static final Logger log = org.slf4j.LoggerFactory.getLogger(Lec01Import.class);
+
+    public static void main(String[] args) {
+        var address = Address.newBuilder()
+                .setCity("Atlanta")
+                .build();
+
+        var car = Car.newBuilder().setBodyStyle(BodyStyle.COUPE).build();
+        var person = Person.newBuilder()
+                .setName("sam")
+                .setAge(12)
+                .setCar(car)
+                .setAddress(address)
+                .build();
+
+        log.info("address: {}", address);
+        log.info("car: {}", car);
+        log.info("person: {}", person);
+    }
+}
