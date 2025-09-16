993898eb8f027c884e2742797d120d7af05a32f9
mrpaulwoods <mr.paul.woods@gmail.com>
2025-09-16 18:37:04 -0500

    feat: enhance GrpcClient to include non-blocking stub for async account balance retrieval with callback

--- src/main/java/org/mrpaulwoods/sec06/GrpcClient.java
+++ src/main/java/org/mrpaulwoods/sec06/GrpcClient.java
@@ -1,9 +1,13 @@
 package org.mrpaulwoods.sec06;
 
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import org.mrpaulwoods.models.sec06.AccountBalance;
 import org.mrpaulwoods.models.sec06.BalanceCheckRequest;
 import org.mrpaulwoods.models.sec06.BankServiceGrpc;
 import org.slf4j.Logger;
+
+import java.time.Duration;
 
 public class GrpcClient {
 
@@ -16,12 +20,38 @@ public class GrpcClient {
                 .usePlaintext()
                 .build();
 
-        var stub = BankServiceGrpc.newBlockingStub(channel);
+        var stub1 = BankServiceGrpc.newBlockingStub(channel);
 
-        var balance = stub.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(2).build());
+        var balance1 = stub1.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(1).build());
 
-        log.info("balance: {}", balance);
+        log.info("balance1: {}", balance1);
 
+        var stub2 = BankServiceGrpc.newStub(channel);
+
+        stub2.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(2).build(),
+                new StreamObserver<>() {
+                    @Override
+                    public void onNext(AccountBalance balance2) {
+                        log.info("balance2: {}", balance2);
+                    }
+
+                    @Override
+                    public void onError(Throwable ex) {
+                        log.error(ex.getMessage(), ex);
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        log.info("completed");
+                    }
+                });
+
+
+        try {
+            Thread.sleep(Duration.ofSeconds(1));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
