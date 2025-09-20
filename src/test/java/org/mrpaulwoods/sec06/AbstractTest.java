68b0f0ccd7a68e271f2ba1fff282d7e9448d4965
mrpaulwoods <mr.paul.woods@gmail.com>
2025-09-20 15:38:45 -0500

    test: add unit test for Transfer bidirectional streaming RPC in Lec05BiDirectionalStreamingTest

--- src/test/java/org/mrpaulwoods/sec06/AbstractTest.java
+++ src/test/java/org/mrpaulwoods/sec06/AbstractTest.java
@@ -5,22 +5,26 @@ import org.junit.jupiter.api.BeforeAll;
 import org.mrpaulwoods.common.AbstractChannelTest;
 import org.mrpaulwoods.common.GrpcServer;
 import org.mrpaulwoods.models.sec06.BankServiceGrpc;
+import org.mrpaulwoods.models.sec06.TransferServiceGrpc;
 
 public abstract class AbstractTest extends AbstractChannelTest {
 
-    private final GrpcServer grpcServer = GrpcServer.create(new BankService());
+    private final GrpcServer grpcServer = GrpcServer.create(new BankService(), new TransferService());
     protected BankServiceGrpc.BankServiceStub bankStub;
     protected BankServiceGrpc.BankServiceBlockingStub bankBlockingStub;
+    protected TransferServiceGrpc.TransferServiceStub transferStub;
 
     @BeforeAll
     public void setup() {
         this.grpcServer.start();
         this.bankStub = BankServiceGrpc.newStub(channel);
         this.bankBlockingStub = BankServiceGrpc.newBlockingStub(channel);
+        this.transferStub = TransferServiceGrpc.newStub(channel);
     }
 
     @AfterAll
     public void stop() {
         this.grpcServer.stop();
     }
+
 }
---
+++ src/test/java/org/mrpaulwoods/sec06/Lec05BiDirectionalStreamingTest.java
@@ -0,0 +1,50 @@
+package org.mrpaulwoods.sec06;
+
+import org.junit.jupiter.api.Test;
+import org.mrpaulwoods.common.ResponseObserver;
+import org.mrpaulwoods.models.sec06.TransferRequest;
+import org.mrpaulwoods.models.sec06.TransferResponse;
+import org.mrpaulwoods.models.sec06.TransferStatus;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Lec05BiDirectionalStreamingTest extends AbstractTest{
+
+    @Test
+    public void transferTest() {
+        var responseObserver = ResponseObserver.<TransferResponse>create();
+        var requestObserver = this.transferStub.transfer(responseObserver);
+
+        var requests = List.of(
+                TransferRequest.newBuilder().setAmount(10).setFromAccount(6).setToAccount(6).build(),
+                TransferRequest.newBuilder().setAmount(110).setFromAccount(6).setToAccount(7).build(),
+                TransferRequest.newBuilder().setAmount(10).setFromAccount(6).setToAccount(7).build(),
+                TransferRequest.newBuilder().setAmount(10).setFromAccount(7).setToAccount(6).build()
+        );
+
+        requests.forEach(requestObserver::onNext);
+        requestObserver.onCompleted();
+
+        responseObserver.await();
+
+        assertEquals(4, responseObserver.getItems().size());
+        validate(responseObserver.getItems().get(0), TransferStatus.REJECTED, 100, 100);
+        validate(responseObserver.getItems().get(1), TransferStatus.REJECTED, 100, 100);
+        validate(responseObserver.getItems().get(2), TransferStatus.COMPLETED, 90, 110);
+        validate(responseObserver.getItems().get(3), TransferStatus.COMPLETED, 100, 100);
+    }
+
+    private void validate(
+            TransferResponse response,
+            TransferStatus status,
+            int fromAccountBalance,
+            int toAccountBalance
+    ) {
+        assertEquals(status, response.getStatus());
+        assertEquals(fromAccountBalance, response.getFromAccount().getBalance());
+        assertEquals(toAccountBalance, response.getToAccount().getBalance());
+    }
+
+}
