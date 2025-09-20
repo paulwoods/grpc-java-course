c92e1bcfc7a12a0037974def98ca92c8f6452215
mrpaulwoods <mr.paul.woods@gmail.com>
2025-09-20 15:02:35 -0500

    test: add unit test for cancel functionality in deposit client-streaming RPC in Lec04ClientStreamingTest

--- src/test/java/org/mrpaulwoods/sec06/Lec04ClientStreamingTest.java
+++ src/test/java/org/mrpaulwoods/sec06/Lec04ClientStreamingTest.java
@@ -1,6 +1,6 @@
 package org.mrpaulwoods.sec06;
 
-import org.checkerframework.checker.units.qual.A;
+import com.google.common.util.concurrent.Uninterruptibles;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mrpaulwoods.common.ResponseObserver;
@@ -8,6 +8,7 @@ import org.mrpaulwoods.models.sec06.AccountBalance;
 import org.mrpaulwoods.models.sec06.DepositRequest;
 import org.mrpaulwoods.models.sec06.Money;
 
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class Lec04ClientStreamingTest extends AbstractTest {
@@ -24,7 +25,7 @@ public class Lec04ClientStreamingTest extends AbstractTest {
                 .build());
 
         // sending stream of deposits
-        IntStream.rangeClosed(1,10)
+        IntStream.rangeClosed(1, 10)
                 .mapToObj(i -> Money.newBuilder().setAmount(10).build())
                 .map(m -> DepositRequest.newBuilder().setMoney(m).build())
                 .forEach(requestObserver::onNext);
@@ -39,6 +40,31 @@ public class Lec04ClientStreamingTest extends AbstractTest {
         Assertions.assertEquals(200, responseObserver.getItems().getFirst().getBalance());
         Assertions.assertNull(responseObserver.getThrowable());
 
+    }
+
+    @Test
+    public void cancelStreamTest() {
+
+        var responseObserver = ResponseObserver.<AccountBalance>create();
+        var requestObserver = this.stub.deposit(responseObserver);
+
+        // initial message - account number
+        requestObserver.onNext(DepositRequest.newBuilder()
+                .setAccountNumber(5)
+                .build());
+
+        Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
+
+        // cancel by sending error
+        requestObserver.onError(new RuntimeException("user canceled"));
+
+        // get the response
+        responseObserver.await();
+
+        Assertions.assertEquals(0, responseObserver.getItems().size());
+
+        Assertions.assertEquals("CANCELLED: Cancelled by client with StreamObserver.onError()", responseObserver.getThrowable().getMessage());
+        Assertions.assertEquals("user canceled", responseObserver.getThrowable().getCause().getMessage());
     }
 
 }
