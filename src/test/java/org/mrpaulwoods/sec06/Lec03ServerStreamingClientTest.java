de33029739d67f167c953b3edf429d7f220f04e3
mrpaulwoods <mr.paul.woods@gmail.com>
2025-09-19 21:33:34 -0500

    test: add unit test for async withdraw server-streaming RPC in Lec03ServerStreamingClientTest

--- src/test/java/org/mrpaulwoods/sec06/Lec03ServerStreamingClientTest.java
+++ src/test/java/org/mrpaulwoods/sec06/Lec03ServerStreamingClientTest.java
@@ -2,11 +2,15 @@ package org.mrpaulwoods.sec06;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mrpaulwoods.common.ResponseObserver;
 import org.mrpaulwoods.models.sec06.Money;
 import org.mrpaulwoods.models.sec06.WithdrawRequest;
 import org.slf4j.Logger;
 
 import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class Lec03ServerStreamingClientTest extends AbstractTest {
 
@@ -21,11 +25,26 @@ public class Lec03ServerStreamingClientTest extends AbstractTest {
 
         Iterator<Money> iterator = this.blockingStub.withdraw(request);
         int count = 0;
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             log.info("received money: {}", iterator.next());
             count++;
         }
         Assertions.assertEquals(2, count);
+    }
+
+    @Test
+    public void asyncClientWithdrawTest() {
+        var request = WithdrawRequest.newBuilder()
+                .setAccountNumber(2)
+                .setAmount(20)
+                .build();
+
+        var observer = ResponseObserver.<Money>create();
+        this.stub.withdraw(request, observer);
+        observer.await();
+        assertEquals(2, observer.getItems().size());
+        assertEquals(10, observer.getItems().getFirst().getAmount());
+        assertNull(observer.getThrowable());
     }
 
 }
