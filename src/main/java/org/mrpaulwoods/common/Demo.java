76e8992ecef6c894b732eb34b6298649e64bf9a6
mrpaulwoods <mr.paul.woods@gmail.com>
2025-09-23 19:41:40 -0500

    build: upgrade grpc to v1.75.0 in pom.xml

--- src/main/java/org/mrpaulwoods/common/Demo.java
+++ src/main/java/org/mrpaulwoods/common/Demo.java
@@ -1,7 +1,6 @@
 package org.mrpaulwoods.common;
 
-import org.mrpaulwoods.sec06.BankService;
-import org.mrpaulwoods.sec06.TransferService;
+import org.mrpaulwoods.sec07.FlowControlService;
 
 /*
     a simple class to start the server with specific services for demo purposes.
@@ -10,7 +9,8 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        GrpcServer.create(new BankService(), new TransferService())
+//        GrpcServer.create(new BankService(), new TransferService())
+        GrpcServer.create(new FlowControlService())
                 .start()
                 .await();
     }
---
+++ src/main/java/org/mrpaulwoods/sec07/FlowControlService.java
@@ -0,0 +1,64 @@
+package org.mrpaulwoods.sec07;
+
+import io.grpc.stub.StreamObserver;
+import org.mrpaulwoods.models.sec07.FlowControlServiceGrpc;
+import org.mrpaulwoods.models.sec07.Output;
+import org.mrpaulwoods.models.sec07.RequestSize;
+import org.slf4j.Logger;
+
+import java.util.stream.IntStream;
+
+public class FlowControlService extends FlowControlServiceGrpc.FlowControlServiceImplBase {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(FlowControlService.class);
+
+    @Override
+    public StreamObserver<RequestSize> getMessages(StreamObserver<Output> responseObserver) {
+        return new RequestHandler(responseObserver);
+
+    }
+
+    private static class RequestHandler implements StreamObserver<RequestSize> {
+
+        private final StreamObserver<Output> responseObserver;
+        private Integer emitted; // the number of messages emitted so far.
+
+        public RequestHandler(StreamObserver<Output> responseObserver) {
+            this.responseObserver = responseObserver;
+            this.emitted = 0;
+        }
+
+        @Override
+        public void onNext(RequestSize requestSize) {
+
+            IntStream.rangeClosed(emitted + 1, 100)
+                    .limit(requestSize.getSize())
+                    .forEach(i -> {
+                        log.info("emitting {}", i);
+                        responseObserver.onNext(Output.newBuilder()
+                                .setValue(i)
+                                .build());
+                    });
+
+            emitted += requestSize.getSize();
+
+            if(emitted >= 100) {
+                log.info("completed");
+                responseObserver.onCompleted();
+            }
+
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+
+        }
+
+        @Override
+        public void onCompleted() {
+            this.responseObserver.onCompleted();
+        }
+
+    }
+
+}
